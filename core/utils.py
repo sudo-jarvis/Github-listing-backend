36ef8e20c8cdf707fa635fa2bdc2a4ba5f9cdd77
Dhruv Singh <dhruv1778@gmail.com>
2023-01-16 19:13:26 +0530

    Minor Fixes

--- core/utils.py
+++ core/utils.py
@@ -5,4 +5,8 @@ def getRepoLanguages(url: str):
     res = requests.get(url)
     if not res.ok:
         return False
-    return res.json()
+    res = res.json()
+    list_lang = []
+    for x in res:
+        list_lang.append(x)
+    return list_lang
